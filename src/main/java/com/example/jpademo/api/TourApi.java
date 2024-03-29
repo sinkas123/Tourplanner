3ea93d48b1f9afd9d30dd5d53d81d90cb9ed2963
Sindi <sindikastrati123@gmail.com>
2024-03-29 22:33:07 +0100

    TourDto

--- src/main/java/com/example/jpademo/api/TourApi.java
+++ src/main/java/com/example/jpademo/api/TourApi.java
@@ -1,5 +1,10 @@
 package com.example.jpademo.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tours")
 public class TourApi {
 
 
---
+++ src/main/java/com/example/jpademo/service/dtos/TourDto.java
@@ -0,0 +1,26 @@
+package com.example.jpademo.service.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Duration;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TourDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String startLocation;
+    private String endLocation;
+    private String transportType;
+    private Double tourDistance;
+    private Duration estimatedTime;
+    private String routeImagePath;
+}
