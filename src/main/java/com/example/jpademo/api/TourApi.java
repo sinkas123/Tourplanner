a52af634a81546421fee064b6b357885409622db
Sindi <sindikastrati123@gmail.com>
2024-03-29 22:23:29 +0100

    TourEntity

--- src/main/java/com/example/jpademo/api/TourApi.java
+++ src/main/java/com/example/jpademo/api/TourApi.java
@@ -2,4 +2,6 @@ package com.example.jpademo.api;
 
 public class TourApi {
 
+
+
 }
--- src/main/java/com/example/jpademo/persistence/entities/TourEntity.java
+++ src/main/java/com/example/jpademo/persistence/entities/TourEntity.java
@@ -1,4 +1,46 @@
 package com.example.jpademo.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Duration;
+
+
+@Entity
+@Table(name="tours")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TourEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 1024)
+    private String description;
+
+    @Column(name = "start_location", nullable = false)
+    private String startLocation;
+
+    @Column(name = "end_location", nullable = false)
+    private String endLocation;
+
+    @Column(name = "transport_type")
+    private String transportType;
+
+    @Column(name = "tour_distance")
+    private Double tourDistance;
+
+    @Column(name = "estimated_time")
+    private Duration estimatedTime;
+
+    @Column(name = "route_image_path")
+    private String routeImagePath;
+
+
 }
